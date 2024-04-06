c24f76155b65172fbccdac73aedb16103f449103
Ashukla011 <shuklavashu9696@gmail.com>
2024-04-06 17:00:25 +0530

    dil food year bise backend

--- config/db.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose=require("mongoose")
-// require("dotenv").config()
-
-
-const  connection=mongoose.connect("mongodb+srv://kavanish011:super30shukla@cluster0.cqhfocy.mongodb.net/Roxiler?retryWrites=true&w=majority")
-
-module.exports={connection};
